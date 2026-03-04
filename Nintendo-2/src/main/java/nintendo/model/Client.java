664cfb41deafb6d7ed6358e52ba90a76c8f60752
DugauguezTheo <theo.dugauguez@gmail.com>
2026-03-04 17:48:39 +0100

    Ajout clients dans main et ajout liste d'achats dans Client

--- Nintendo-2/src/main/java/nintendo/model/Client.java
+++ Nintendo-2/src/main/java/nintendo/model/Client.java
@@ -1,17 +1,21 @@
 package nintendo.model;
 
+import java.util.List;
+
 public class Client {
   protected String nom;
   protected String prenom;
+  protected List<Achat> listeAchats;
 
   @Override
   public String toString() {
     return "Client [nom=" + nom + ", prenom=" + prenom + "]";
   }
 
-  public Client(String prenom, String nom) {
+  public Client(String prenom, String nom, List<Achat> listeAchats) {
     this.prenom = prenom;
     this.nom = nom;
+    this.listeAchats = listeAchats;
   }
 
   public String getPrenom() {
--- Nintendo-2/src/test/java/nintendo/test/Test.java
+++ Nintendo-2/src/test/java/nintendo/test/Test.java
@@ -1,5 +1,13 @@
 package nintendo.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import nintendo.model.Achat;
+import nintendo.model.Adresse;
+import nintendo.model.Boutique;
+import nintendo.model.Client;
+
 public class Test 
 {
 	public static class Console 
@@ -35,12 +43,20 @@ public class Test
 	public static void main(String[] args) 
 	{
 		 Console switchConsole = new Console("Nintendo Switch");
-
+		 
+		 Adresse adresse = new Adresse(1,"Rue de la Paix", "Paris");
+		 Boutique boutique1 = new Boutique("Boutique 1", adresse);
+		 
+		 List<Achat> achats = new ArrayList();
+		 
 	     Jeu jeu1 = new Jeu("Zelda", switchConsole);
 	     Jeu jeu2 = new Jeu("R6", switchConsole);
 	     Jeu jeu3 = new Jeu("Rocket league", switchConsole);
 	     Jeu jeu4 = new Jeu("Pokemon", switchConsole);
 	     Jeu jeu5 = new Jeu("Super Smash Bros", switchConsole);
+	     
+	     Client client1 = new Client("Dugauguez","Theo", achats);
+	     Client client2 = new Client("Cambray","Martin", achats);
 
 	     System.out.println(jeu1);
 	     System.out.println(jeu2);
