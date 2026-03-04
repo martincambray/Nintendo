9c69fa18cb4178a96d7949b01836dcb70ca9f3e4
martin.cmb <mcambray.pro@gmail.com>
2026-03-04 17:35:12 +0100

    oups

--- Nintendo-2/src/main/java/nintendo/model/Achat.java
+++ Nintendo-2/src/main/java/nintendo/model/Achat.java
@@ -3,23 +3,18 @@ import java.time.LocalDate;
 
 public class Achat 
 {
-
     private Jeu jeu;
     private LocalDate date;
     private double prix;
-
     public Achat(Jeu jeu, LocalDate date, double prix) 
     {
         this.jeu = jeu;
         this.date = date;
         this.prix = prix;
     }
-
     @Override
     public String toString() 
     {
-        return "Achat de " + jeu +
-               " le " + date +
-               " pour " + prix + "€";
+        return "Achat de " + jeu + " le " + date + " pour " + prix + "€";
     }
 }
