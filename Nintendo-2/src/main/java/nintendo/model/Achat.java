0ae527f561060ad3d9d21ae13fe26a5bf8e7657c
martin.cmb <mcambray.pro@gmail.com>
2026-03-04 17:31:55 +0100

    Vous devez créer la classe Achat(Jeu, date, prix)

---
+++ Nintendo-2/src/main/java/nintendo/model/Achat.java
@@ -0,0 +1,25 @@
+package nintendo.model;
+import java.time.LocalDate;
+
+public class Achat 
+{
+
+    private Jeu jeu;
+    private LocalDate date;
+    private double prix;
+
+    public Achat(Jeu jeu, LocalDate date, double prix) 
+    {
+        this.jeu = jeu;
+        this.date = date;
+        this.prix = prix;
+    }
+
+    @Override
+    public String toString() 
+    {
+        return "Achat de " + jeu +
+               " le " + date +
+               " pour " + prix + "€";
+    }
+}
